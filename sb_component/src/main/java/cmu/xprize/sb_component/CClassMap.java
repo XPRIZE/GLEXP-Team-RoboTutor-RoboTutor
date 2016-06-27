53e07ebedfbdaefee858a6ca6a60121c1b945da4
Jacky <kuoyichun1102@gmail.com>
2016-06-26 22:02:21 -0400

    custom attribute for col_num, size, score

---
+++ sb_component/src/main/java/cmu/xprize/sb_component/CClassMap.java
@@ -0,0 +1,28 @@
+package cmu.xprize.sb_component;
+
+import java.util.HashMap;
+
+
+/**
+ * Created by Kevin on 2/29/2016.
+ */
+public class CClassMap {
+
+    static public HashMap<String, Class> classMap = new HashMap<String, Class>();
+
+    //
+    // This is used to map "type" (class names) used in json HashMap specs to real classes
+
+    static {
+        classMap.put("Sb_Data", CSb_Data.class);
+
+        classMap.put("string", String.class);
+        classMap.put("bool", Boolean.class);
+        classMap.put("int", Integer.class);
+        classMap.put("float", Float.class);
+        classMap.put("byte", Byte.class);
+        classMap.put("long", Long.class);
+        classMap.put("short", Short.class);
+        classMap.put("object", Object.class);
+    }
+}
