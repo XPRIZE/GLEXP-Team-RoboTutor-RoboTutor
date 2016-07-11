7a0b9f7aae000faaab5026e6b74ba037865b1545
timshell <magrawa1@swarthmore.edu>
2016-07-11 15:34:43 -0400

    working on RoboTutor - need to fix animation graph

--- ar_component/src/main/java/cmu/xprize/ar_component/CAr_Component.java
+++ ar_component/src/main/java/cmu/xprize/ar_component/CAr_Component.java
@@ -82,6 +82,9 @@ public class CAr_Component extends LinearLayout implements ILoadableObject {
 
     public void init(Context context, AttributeSet attrs) {
 
+        setOrientation(VERTICAL);
+        setOnClickListener(clickListener);
+
         mContext = context;
 
         if (attrs != null) {
@@ -456,6 +459,11 @@ public class CAr_Component extends LinearLayout implements ILoadableObject {
             viewGroup.setClipToPadding(enabled);
             v = viewGroup;
         }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
     }
 
     /**
