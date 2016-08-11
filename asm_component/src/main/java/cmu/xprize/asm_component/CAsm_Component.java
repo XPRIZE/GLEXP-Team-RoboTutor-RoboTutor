b714fcf357d764f49a7a4d54f4a2e85d4ff4dffa
TingguangYin <tingguangyin@gmail.com>
2016-08-12 06:06:54 +0800

    Fix bug
    Now, user can't change the digits, which have been input correctly.

--- asm_component/src/main/java/cmu/xprize/asm_component/CAsm_Component.java
+++ asm_component/src/main/java/cmu/xprize/asm_component/CAsm_Component.java
@@ -177,7 +177,14 @@ public class CAsm_Component extends LinearLayout implements ILoadableObject, IEv
 
     public void nextDigit() {
 
-        digitIndex--;
+        for (CAsm_Alley alley: allAlleys) {
+            try {
+                CAsm_Text text = alley.getTextLayout().getText(digitIndex);
+                if (text.isWritable) {text.setWritable(false); }
+            } catch (NullPointerException e) { continue;}
+        }
+
+        digitIndex--; 
 
         mechanics.nextDigit();
 
