8b77c28fbe781b5c7c9327d7d5bc12abb87e34b9
Kevin DeLand <kevin.deland@gmail.com>
2018-10-28 13:02:47 -0400

    update QUICK_DEBUG to be false

--- app/src/main/java/cmu/xprize/robotutor/RoboTutor.java
+++ app/src/main/java/cmu/xprize/robotutor/RoboTutor.java
@@ -104,7 +104,7 @@ public class RoboTutor extends Activity implements IReadyListener, IRoboTutor {
 //    private static final String debugTutorVariant = "placevalue";
 //    private static final String debugTutorId = "place.value:1";
 //    private static final String debugTutorFile = "[file]place.value__pv-11..99.2D.diff0.3.json";
-    private static final boolean QUICK_DEBUG = true;
+    private static final boolean QUICK_DEBUG = false;
     private static final String debugTutorVariant = "write.sen.copy.ltr";
     private static final String debugTutorId = "write.wrd:story_1_1";
     private static final String debugTutorFile = "[encfolder]write.sen.copy.ltr_story_1.noerror.1.2.json";
