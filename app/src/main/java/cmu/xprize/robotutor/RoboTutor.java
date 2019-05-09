3cd8ab0b941eae21e9f6626bcbb4776758b8994c
Kevin DeLand <kevin.deland@gmail.com>
2019-05-09 07:26:17 -0400

    DEBUG: move list of QuickDebugTutor into separate class

--- app/src/main/java/cmu/xprize/robotutor/RoboTutor.java
+++ app/src/main/java/cmu/xprize/robotutor/RoboTutor.java
@@ -39,9 +39,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
 import cmu.xprize.comp_logging.CErrorManager;
 import cmu.xprize.comp_logging.CLogManager;
@@ -60,6 +58,7 @@ import cmu.xprize.robotutor.tutorengine.CMediaController;
 import cmu.xprize.robotutor.tutorengine.CTutorAssetManager;
 
 import cmu.xprize.robotutor.tutorengine.QuickDebugTutor;
+import cmu.xprize.robotutor.tutorengine.QuickDebugTutorList;
 import cmu.xprize.robotutor.tutorengine.util.CAssetObject;
 import cmu.xprize.util.CDisplayMetrics;
 import cmu.xprize.util.CLoaderView;
@@ -105,164 +104,8 @@ public class RoboTutor extends Activity implements IReadyListener, IRoboTutor {
 
 
     // VARIABLES FOR QUICK DEBUG LAUNCH
-//    private static final String debugTutorVariant = "placevalue";
-//    private static final String debugTutorId = "place.value:1";
-//    private static final String debugTutorFile = "[file]place.value__pv-11..99.2D.diff0.3.json";
     private static final boolean QUICK_DEBUG = false;
     private static final String QUICK_DEBUG_TEST_KEY = "akira:missing_audio";
-
-    // BUGS to check...
-    // in the future, this could be a cool debugging screen that pops up and leads you through bugs
-    // there could also be an in-app bug-reporting system
-    private static Map<String, QuickDebugTutor> bugMap = new HashMap<>();
-    static {
-
-        // --------
-        // RESOLVED
-        // --------
-        QuickDebugTutor bug1 = new QuickDebugTutor("write.wrd.dic",
-                "write.wrd.dic:word.dolch_3rd_grade",
-                "[file]write.wrd.dic_word.dolch_3rd_grade.json");
-        bug1.setComment("Missing the word 'if' and hangs");
-        bugMap.put("write:missing_word", bug1);
-        bug1.resolve(); // FIXED
-
-        QuickDebugTutor bug2 = new QuickDebugTutor(
-                "numcompare",
-                "numcompare:1d",
-                "[file]numcompare_1d.json"
-                );
-        bug2.setComment("Missing audio");
-        bug2.setComment("assets/audio/en/cmu/xprize/nd/{{SNumDiscr.digitCompare}}.mp3");
-        bug2.setComment("assets/audio/en/cmu/xprize/nd/So the bigger number is.mp3");
-        bugMap.put("nd:missing_prompts", bug2);
-        bug2.resolve(); // FIXED
-
-        QuickDebugTutor bug5 = new QuickDebugTutor(
-                "place.value",
-                "place.value:pv-100..499.3D.diff0.17",
-                "[file]place.value_pv-100..499.3D.diff0.17.json"
-        );
-        bug5.setComment("Missing audio 'tap inside the box'; stimulus box is ugly; second screen missing number audio.");
-        bug5.setLocation("CCountingXComponent");
-        bugMap.put("place.value:many_bugs", bug5);
-        bug5.resolve(); // FIXED
-
-        QuickDebugTutor bug6 = new QuickDebugTutor(
-                "place.value",
-                "place.value:pv-100..499.3D.diff2.26",
-                "[file]place.value_pv-100..499.3D.diff2.26.json"
-        );
-        bug6.setComment("Missing audio on second screen.");
-        bug6.setPriority(QuickDebugTutor.Priority.MUST);
-        bugMap.put("place.value:missing_audio", bug6);
-        bug6.resolve(); // FIXED
-
-        QuickDebugTutor bug11 = new QuickDebugTutor(
-                "place.value",
-                "place.value:pv-100..499.3D.diff2.26",
-                "[file]place.value_pv-100..499.3D.diff2.26.json"
-        );
-        bug11.setComment("countx audio should just say 'please tap' if place value.");
-        bug11.setLocation("countingx/animator_graph.json");
-        bug11.setPriority(QuickDebugTutor.Priority.SHOULD);
-        bugMap.put("place.value:wrong_audio", bug11);
-        bug11.resolve(); // FIXED
-
-        QuickDebugTutor bug3 = new QuickDebugTutor(
-                "bpop.gl",
-                "bpop.gl:dot.0..9.GL_SD_OFF1_L.bub2.6",
-                "[file]bpop.gl_dot.0..9.GL_SD_OFF1_L.bub2.6.json"
-        );
-        bug3.setComment("Should say 'tap the bigger number' or 'tap the smaller number");
-        bug3.setLocation("bubble_pop/animator_graph.json");
-        bug3.setPriority(QuickDebugTutor.Priority.MUST);
-        bugMap.put("bpop.gl:wrong_prompt", bug3);
-        bug3.resolve(); // FIXED
-
-        QuickDebugTutor bug4 = new QuickDebugTutor(
-                "bpop.mn",
-                "bpop.mn:0..9.MN-SD-UP-OFF1-BL1.incr.4",
-                "[file]bpop.mn_0..9.MN-SD-UP-OFF1-BL1.incr.4.json"
-        );
-        bug4.setComment("Should say 'tap the missing number'");
-        bug4.setLocation("bubble_pop/animator_graph.json");
-        bug4.setPriority(QuickDebugTutor.Priority.MUST);
-        bugMap.put("bpop.mn:wrong_prompt", bug4);
-        bug4.resolve(); // FIXED
-
-        QuickDebugTutor bug7 = new QuickDebugTutor(
-                "story.parrot",
-                "story.parrot::ea2eh_wb_2",
-                "[encfolder]ea2eh_wb_2"
-        );
-        bug7.setComment("Should say 'Read after me'");
-        bugMap.put("story.parrot:missing_prompt", bug7);
-        bug7.resolve(); // FIXED
-
-        QuickDebugTutor bug8 = new QuickDebugTutor(
-                "story.pic.hear",
-                "story.pic.hear::1_13",
-                "[encfolder]1_13"
-        );
-        bug8.setComment("Missing audio prompts e.g. 'Which picture?'");
-        bugMap.put("story.pic:missing_prompt", bug8);
-        bug8.resolve(); // FIXED
-
-        QuickDebugTutor bug12 = new QuickDebugTutor(
-                "bpop.gl",
-                "bpop.gl:num.100..900.GL_DD_OFF100_L.bub2.30.json",
-                "[file]bpop.gl_num.100..900.GL_DD_OFF100_L.bub2.30.json");
-        bug12.setComment("Audio not playing");
-        bugMap.put("bpop.gl:multidigit_audio", bug12);
-        bug12.setPriority(QuickDebugTutor.Priority.MUST);
-        bug12.setLocation("bubble_pop/animator_graph.json");
-        bug12.resolve(); // FIXED
-
-        QuickDebugTutor bug13 = new QuickDebugTutor(
-                "bpop.num",
-                "bpop.num:1..4.by.1.asc.q2q.AV.mc.1",
-                "[file]bpop.num_1..4.by.1.asc.q2q.AV.mc.1.json"
-        );
-        bug13.setComment("Number audio not playing");
-        bugMap.put("bpop.num:missing_audio", bug13);
-        bug13.setPriority(QuickDebugTutor.Priority.MUST);
-        bug13.resolve(); // FIXED
-
-        // -------
-        // TO FIX
-        // -------
-        // STATUS: review
-        QuickDebugTutor bug9 = new QuickDebugTutor(
-                "numcompare",
-                "numcompare:1d",
-                "[file]numcompare_1d.json"
-        );
-        bug9.setComment("Audio prompts should not say 'First tap on the ones' if there are only ones.");
-        bug9.setPriority(QuickDebugTutor.Priority.COULD);
-        bugMap.put("numcompare:wordy_prompt", bug9);
-
-        // STATUS: review
-        QuickDebugTutor bug10 = new QuickDebugTutor(
-                "place.value",
-                "place.value:pv-100..499.3D.diff2.26",
-                "[file]place.value_pv-100..499.3D.diff2.26.json"
-        );
-        bug10.setComment("'Good job' audio is overlapping on second screen.");
-        bug10.setPriority(QuickDebugTutor.Priority.COULD);
-        bugMap.put("place.value:overlapping_audio", bug10);
-
-        QuickDebugTutor bug14 = new QuickDebugTutor(
-                "akira",
-                "akira:10..100.by.within.des",
-                "[file]akira_10..100.by.within.des.json"
-        );
-        bug14.setComment("Akira prompt number not playing");
-        bug14.setLocation("akira/animator_graph.json --> INSTRUCT_3V");
-        bugMap.put("akira:missing_audio", bug14);
-
-
-    }
 
     public static final String MATRIX_FILE = "dev_data.open.json";
 
@@ -766,7 +609,7 @@ public class RoboTutor extends Activity implements IReadyListener, IRoboTutor {
 
         Log.d(TCONST.DEBUG_GRAY_SCREEN_TAG, "xx: startSessionManager in 'onStartTutor'");
 
-        QuickDebugTutor debugMe = bugMap.get(QUICK_DEBUG_TEST_KEY);
+        QuickDebugTutor debugMe = QuickDebugTutorList.toFixBugMap.get(QUICK_DEBUG_TEST_KEY);
         // CTutorEngine.quickLaunch(debugTutorVariant, debugTutorId, debugTutorFile);
         CTutorEngine.quickLaunch(debugMe.tutorVariant, debugMe.tutorId, debugMe.tutorFile);
 
